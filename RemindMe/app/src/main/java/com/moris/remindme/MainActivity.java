1c7cb3d15f04038c8c272935eaa50da8948ab573
MorisW <160273@gmail.com>
2018-07-24 01:41:10 +0500

    Главное активити

---
+++ RemindMe/app/src/main/java/com/moris/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package com.moris.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.qoobico.remindme.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maun);
+    }
+}
