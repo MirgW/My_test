ca7300b844ba2aef2fbbfced39f4457b2d989b62
MorisW <160273@gmail.com>
2018-07-27 01:20:47 +0500

    NavigationView

--- RemindMe/app/src/main/java/com/moris/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/moris/remindme/MainActivity.java
@@ -3,18 +3,20 @@ package com.moris.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.moris.remindme.R;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
  private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maun);
 
