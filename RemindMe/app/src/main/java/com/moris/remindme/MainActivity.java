90422b2d1dae3bc1e1fdf9582b7bf965633c23ec
MorisW <160273@gmail.com>
2018-07-27 00:15:29 +0500

    Таск и сеарч

--- RemindMe/app/src/main/java/com/moris/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/moris/remindme/MainActivity.java
@@ -3,13 +3,34 @@ package com.moris.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-import com.qoobico.remindme.R;
+import com.moris.remindme.R;
+
 
 public class MainActivity extends Activity {
+
+ private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maun);
+
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.inflateMenu(R.menu.menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
     }
 }
