0a39b7cb95fdd212fcf39e153a9ecbffa389a46b
MorisW <160273@gmail.com>
2018-07-29 01:10:36 +0500

    Cards

--- RemindMe/app/src/main/java/com/moris/remindme/fragment/HistoryFragment.java
+++ RemindMe/app/src/main/java/com/moris/remindme/fragment/HistoryFragment.java
@@ -4,14 +4,25 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.test.mock.MockContext;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.moris.remindme.R;
+import com.moris.remindme.adapter.RemindListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.Mac;
+
+import dto.RemindDTO;
 
 public class HistoryFragment extends AbstractTabFragment {
-    private static final int LAYOUT =R.layout.fragment_example;
+    private static final int LAYOUT =R.layout.fragment_history;
 
 
     @Nullable
@@ -37,6 +48,20 @@ public class HistoryFragment extends AbstractTabFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT,container,false);
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.recyclerView);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new RemindListAdapter(creatMockData()));
         return view;
+    }
+
+    private List<RemindDTO> creatMockData() {
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        data.add(new RemindDTO("Item 6"));
+        return data;
     }
 }
