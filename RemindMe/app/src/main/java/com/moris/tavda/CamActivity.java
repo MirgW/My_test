df3688bdae6db5e4706d05eae36fe10fc32e0028
MorisW <160273@gmail.com>
2018-08-17 23:05:10 +0500

    - Камера

---
+++ RemindMe/app/src/main/java/com/moris/tavda/CamActivity.java
@@ -0,0 +1,27 @@
+package com.moris.tavda;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CamActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam);
+
+    }
+}
