07705d088688a05410df03f3b38a66e6c42904e3
W <reader2000r@gmail.com>
2021-12-12 02:33:51 +0500

    Макет, грабер

--- RemindMe/app/src/main/java/com/moris/tavda/Data/DTODataSource.java
+++ RemindMe/app/src/main/java/com/moris/tavda/Data/DTODataSource.java
@@ -25,19 +25,22 @@ public class DTODataSource extends PageKeyedDataSource<Long, RemindDTO> {
         try {
             document = Jsoup.connect("http://www.adm-tavda.ru/node?page=0").get();
             Elements elements = document.select(".node.story.promote");
-//                String dd;
+            String dd;
             data.clear();
             for (Element element : elements) {
 //                    dd=element.select("h2.art-postheader>a").attr("href");
-                data.add(new RemindDTO(element.select("h2").text(), element.select("p").text(), element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
+                if ((element.select("div.art-article>div").text() != "") & (element.select("div.art-article>div").size() > 1)) {
+                    dd = element.select("div.art-article>div").get(0).text() + element.select("p").text();
+                } else dd = element.select("p").text();
+//                dd = element.select("div.art-article>div").size() + " - " + dd;
+                data.add(new RemindDTO(element.select("h2").text(), dd, element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
             }
-
         } catch (IOException e) {
             e.printStackTrace();
             data.add(new RemindDTO("Нет соединения с источником", "", "", "Проверьте подключение к Wi-Fi или сотовой сети", ""));
         }
 //        List<RemindDTO> result = data;
-callback.onResult(data,null,1L);
+        callback.onResult(data, null, 1L);
     }
 
     @Override
@@ -46,23 +49,27 @@ callback.onResult(data,null,1L);
         final Document document;
 
         try {
-            document = Jsoup.connect("http://www.adm-tavda.ru/node?page="+params.key.toString()).get();
+            document = Jsoup.connect("http://www.adm-tavda.ru/node?page=" + params.key.toString()).get();
             Elements elements = document.select(".node.story.promote");
-//                String dd;
+            String dd;
             data.clear();
             for (Element element : elements) {
 //                    dd=element.select("h2.art-postheader>a").attr("href");
-                data.add(new RemindDTO(element.select("h2").text(), element.select("p").text(), element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
-            }
+//                data.add(new RemindDTO(element.select("h2").text(), element.select("p").text(), element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
 
+                if ((element.select("div.art-article>div").text() != "") & (element.select("div.art-article>div").size() > 1)) {
+                    dd = element.select("div.art-article>div").get(0).text() + element.select("p").text();
+                } else dd = element.select("p").text();
+//                dd = element.select("div.art-article>div").size() + " - " + dd;
+                data.add(new RemindDTO(element.select("h2").text(), dd, element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
+            }
         } catch (IOException e) {
             e.printStackTrace();
-//            data.add(new RemindDTO("Нет соединения с источником", "Источник не доступен", "", "Нет соеденения", "http://www.1.ru"));
             data.add(new RemindDTO("Нет соединения с источником", "", "", "Проверьте подключение к Wi-Fi или сотовой сети", ""));
         }
 //        List<RemindDTO> result = data;
-        Page=Page-1;
-        callback.onResult(data,params.key.longValue()-1L);
+        Page = Page - 1;
+        callback.onResult(data, params.key.longValue() - 1L);
     }
 
     @Override
@@ -71,22 +78,26 @@ callback.onResult(data,null,1L);
         final Document document;
 
         try {
-            document = Jsoup.connect("http://www.adm-tavda.ru/node?page="+params.key.toString()).get();
+            document = Jsoup.connect("http://www.adm-tavda.ru/node?page=" + params.key.toString()).get();
             Elements elements = document.select(".node.story.promote");
-//                String dd;
+            String dd;
             data.clear();
             for (Element element : elements) {
 //                    dd=element.select("h2.art-postheader>a").attr("href");
-                data.add(new RemindDTO(element.select("h2").text(), element.select("p").text(), element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
-            }
+//                data.add(new RemindDTO(element.select("h2").text(), element.select("p").text(), element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
 
+                if ((element.select("div.art-article>div").text() != "") & (element.select("div.art-article>div").size() > 1)) {
+                    dd = element.select("div.art-article>div").get(0).text() + element.select("p").text();
+                } else dd = element.select("p").text();
+//                dd = element.select("div.art-article>div").size() + " - " + dd;
+                data.add(new RemindDTO(element.select("h2").text(), dd, element.select("img").attr("src"), element.select("span.art-postdateicon").text(), element.select("h2.art-postheader>a").attr("href")));
+            }
         } catch (IOException e) {
             e.printStackTrace();
-//            data.add(new RemindDTO("Нет соединения с источником", "Источник не доступен", "", "Нет соеденения", "http://www.1.ru"));
             data.add(new RemindDTO("Нет соединения с источником", "", "", "Проверьте подключение к Wi-Fi или сотовой сети", ""));
         }
 //        List<RemindDTO> result = data;
-        Page=Page+1;
-        callback.onResult(data,params.key.longValue()+1L);
+        Page = Page + 1;
+        callback.onResult(data, params.key.longValue() + 1L);
     }
 }
