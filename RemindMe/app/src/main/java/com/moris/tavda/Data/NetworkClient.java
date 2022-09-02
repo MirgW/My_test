7e930e66b4a4328302087783d8d8eceedb4eb4fb
W <reader2000r@gmail.com>
2022-09-03 03:54:18 +0500

    Обратная связь 2 - dns - gradle-7.5.1 - обновленные библиотека - TextInputLayout - скрытый

--- RemindMe/app/src/main/java/com/moris/tavda/Data/NetworkClient.java
+++ RemindMe/app/src/main/java/com/moris/tavda/Data/NetworkClient.java
@@ -4,8 +4,15 @@ import android.content.Context;
 
 import com.moris.tavda.BuildConfig;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import androidx.annotation.NonNull;
+import okhttp3.Dns;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
@@ -20,7 +27,8 @@ import retrofit2.http.Part;
 
 public class NetworkClient {
 //    private static final String BASE_URL = "http://192.168.1.7:8070";
-      private static final String BASE_URL = "http://192.168.1.4:8070";
+private static final String BASE_URL = "http://tuory.ru";
+//    private static final String BASE_URL = "http://192.168.1.4:8070";
 //      private static final String BASE_URL = "http://tavda.great-site.net";
 //    private static final String BASE_URL = "http://db4free.net:3360";
 //    private static final String BASE_URL = "https://oddball-stomachs.000webhostapp.com";
@@ -38,6 +46,16 @@ public class NetworkClient {
                     .connectTimeout(1, TimeUnit.MINUTES) // connect timeout
                     .writeTimeout(1, TimeUnit.MINUTES) // write timeout
                     .readTimeout(1, TimeUnit.MINUTES) // read timeout
+                    .dns(new Dns() {
+                        @NonNull
+                        @Override
+                        public List<InetAddress> lookup(@NonNull String s) throws UnknownHostException {
+                            InetAddress ip1 = Inet4Address.getByName("31.31.196.26");
+//                            InetAddress ip2 = Inet4Address.getByName("192.168.1.9");
+                            return Arrays.asList(ip1);
+                            //                            return null;
+                        }
+                    })
                     .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
